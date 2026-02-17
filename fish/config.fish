bbb28e9925ff24f0f5407186c8558a6e4e26e80d
Richieeefff <ikhsannul201@gmail.com>
2026-02-17 12:06:23 +0700

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,18 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 alias musicsync "adbsync push /home/hypatiac/Music/. /sdcard/Music/Seeker/nicotine/"
 
+function cd
+    builtin cd $argv
+    eza --grid --icons
+end
+
+zoxide init fish | source
+
+function z --wraps=__zoxide_z
+    __zoxide_z $argv
+    eza --grid --icons
+end
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
