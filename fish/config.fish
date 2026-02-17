8a0be8acc10b8ed2386215b0324b2e3425a640df
Richieeefff <ikhsannul201@gmail.com>
2026-02-17 11:17:37 +0700

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+alias musicsync "adbsync push /home/hypatiac/Music/. /sdcard/Music/Seeker/nicotine/"
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
